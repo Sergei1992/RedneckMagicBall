1855aa3648cbf86ce1db3c6ad8afbae62b7217f0
Sergei Ershov <ershov.s.sergei@gmail.com>
2018-09-30 21:26:11 -0500

    first commit

---
+++ app/src/main/java/com/sergei/s/ershov/redneckmagicball/MainActivity.java
@@ -0,0 +1,67 @@
+package com.sergei.s.ershov.redneckmagicball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txt;
+    Button btn;
+    Random rng = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt = (TextView) findViewById(R.id.myText);
+        btn = (Button) findViewById(R.id.myButton);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int randomNumber = rng.nextInt(8) + 1;
+
+                switch (randomNumber) {
+                    case 1:
+                        txt.setText("Heal No");
+                        break;
+
+                    case 2:
+                        txt.setText("Heal Yes");
+                        break;
+
+                    case 3:
+                        txt.setText("I tell you what, don`t");
+                        break;
+
+                    case 4:
+                        txt.setText("Just tri it");
+                        break;
+
+                    case 5:
+                        txt.setText("Wut?");
+                        break;
+
+                    case 6:
+                        txt.setText("I doubt it");
+                        break;
+
+                    case 7:
+                        txt.setText("Imma alredy drunk");
+                        break;
+
+                    case 8:
+                        txt.setText("Don`t be a pussy");
+                        break;
+
+                }
+            }
+        });
+
+    }
+}
