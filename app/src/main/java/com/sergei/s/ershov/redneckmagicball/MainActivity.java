dc195fae30cd4e1dc08122a18f40cb1d01d1e1ea
Sergei Ershov <ershov.s.sergei@gmail.com>
2018-10-08 17:34:42 -0500

    fix an error

--- app/src/main/java/com/sergei/s/ershov/redneckmagicball/MainActivity.java
+++ app/src/main/java/com/sergei/s/ershov/redneckmagicball/MainActivity.java
@@ -1,11 +1,12 @@
 package com.sergei.s.ershov.redneckmagicball;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 import java.util.Random;
 
@@ -25,8 +26,11 @@ public class MainActivity extends AppCompatActivity {
                 "Wats up Redneck!", Toast.LENGTH_LONG);
         toast.show();
 
+       final Animation fade = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.alpha);
+
         img = (ImageView) findViewById(R.id.myImage);
         btn = (Button) findViewById(R.id.myButton);
+
 
 
         btn.setOnClickListener(new View.OnClickListener() {
@@ -37,34 +41,42 @@ public class MainActivity extends AppCompatActivity {
                 switch (randomNumber) {
                     case 1:
                         img.setImageResource(R.drawable.healno);
+                        img.startAnimation(fade);
                         break;
 
                     case 2:
                         img.setImageResource(R.drawable.healyes);
+                        img.startAnimation(fade);
                         break;
 
                     case 3:
                         img.setImageResource(R.drawable.dont);
+                        img.startAnimation(fade);
                         break;
 
                     case 4:
                         img.setImageResource(R.drawable.tryit);
+                        img.startAnimation(fade);
                         break;
 
                     case 5:
                         img.setImageResource(R.drawable.wut);
+                        img.startAnimation(fade);
                         break;
 
                     case 6:
                         img.setImageResource(R.drawable.doubit);
+                        img.startAnimation(fade);
                         break;
 
                     case 7:
                         img.setImageResource(R.drawable.drunk);
+                        img.startAnimation(fade);
                         break;
 
                     case 8:
                         img.setImageResource(R.drawable.pussy);
+                        img.startAnimation(fade);
                         break;
 
                 }
